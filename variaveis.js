753540743906d9ee7a8b2559ccd252bb2dde9b3c
João Luis <joaoluisd2005@gmail.com>
2025-05-23 22:36:22 -0300

    update

--- variaveis.js
+++ variaveis.js
@@ -1,6 +1,18 @@
-let nome = "Joao"; // variavel string
-let numero = 19; // variavel numerica
-let falso = false; // booleana false
-let verdadeiro = true; // booleana true
+//let nome = "Joao"; // variavel string
+//let numero = 19; // variavel numerica
+//let falso = false; // booleana false
+//let verdadeiro = true; // booleana true
 
-const PI = 3.1415
+//const PI = 3.1415 //constante
+
+let num1 = Number(prompt("digite um numero: "));
+let num2 = Number(prompt("digite outro numero"));
+let resultado = num1 + num2;
+console.log(resultado);
+document.writeln("O resultado e: ",+ resultado);
+//alert(resultado);
+
+//console.log(prompt("digite um numero: "));
+
+//console.log("Uma frase");
+//console.log(1+2);
