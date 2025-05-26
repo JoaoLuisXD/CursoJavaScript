e64b78e262f280b152cad00d5f47f22fb2165319
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-05-26 10:32:35 -0300

    update

---
+++ conditinal_structs.js
@@ -0,0 +1,24 @@
+/*let n1 = Number(prompt("digite o primeiro numero"));
+let n2 = Number(prompt("digite o segundo numero"));
+if(n1 > n2)
+{
+    console.log(`${n1} e maior que ${n2}`);
+}
+else if(n1 < n2)console.log(`${n2} e maior que ${n1}`);
+else if(n1 == n2)console.log(`${n1} e ${n2} sao iguais`);*/
+
+let mode = "dark";
+console.log(mode == "dark" ? "eh" : "naoeh");
+
+
+/*switch(mode)
+{
+    case "dark": document.querySelector("body").style.backgroundColor = "#222222";
+    break;
+    case "red": document.querySelector("body").style.backgroundColor = "#FF0022";
+    break;
+    case "blue": document.querySelector("body").style.backgroundColor = "#2200FF"
+    break;
+    default: document.querySelector("body").style.backgroundColor = "#FF00FF";
+
+}*/
