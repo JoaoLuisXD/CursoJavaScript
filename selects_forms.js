feac8c265f59fc7d849dcc618ed56093647c243e
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-05-27 21:59:58 -0300

    update

--- selects_forms.js
+++ selects_forms.js
@@ -1,14 +1,14 @@
 let meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
-
+// define meses com os nomes dos meses do ano.
 for(let i = 1; i < 32; i++)
 {
-    dia.innerHTML += `<option>${i}</option>`;
+    dia.innerHTML += `<option>${i}</option>`; //Adicione essa nova <option> dentro do conteúdo HTML do <select> chamado dia.
 }
 for(let j = 0; j < 12; j++)
 {
-    mes.innerHTML += `<option>${meses[j]}</option>`;
+    mes.innerHTML += `<option>${meses[j]}</option>`; //Adicione essa nova <option> dentro do conteúdo HTML do <select> chamado mes.
 }
 for(let k = 2025; k > 1974; k--)
 {
-    ano.innerHTML += `<option>${k}</option>`;
+    ano.innerHTML += `<option>${k}</option>`; //Adicione essa nova <option> dentro do conteúdo HTML do <select> chamado ano.
 }
